ab5e9583886c7f943045fe52320ecbd8370a6f15
cewinharhar <kevin.yar@outlook.com>
2023-03-16 17:27:53 +0100

    create distance calculateor

--- src/main_pyroprolex.py
+++ src/main_pyroprolex.py
@@ -9,10 +9,18 @@ def main_pyroprolex():
 #--------------------- pyrosetta relax
 
 import pyrosetta
+from pyrosetta.toolbox import cleanATOM
 
 
+pyrosetta.init("-ignore_unrecognized_res 1 -ex1 -ex2aro")
 
+#load from PDB databank
+start_pose = pyrosetta.toolbox.rcsb.pose_from_rcsb("1AB1", ATOM=True, CRYS=False)
+pose = start_pose.clone()
+scorefxn = pyrosetta.create_score_function("ref2015_cart.wts")
 
+#load from local PDB
+pose = pyrosetta.pose_from_file("/home/cewinharhar/GITHUB/reincatalyze/data/processed/3D_pred/testRun/aKGD_FE_oxo.cif")
 
 
 #--------------------- Pymol relax
