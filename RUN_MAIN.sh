1a9cc555bd48f701a83918d8d3c308adb6c5ea08
cewinharhar <kevin.yar@outlook.com>
2023-05-12 13:27:23 +0200

    changes

--- RUN_MAIN.sh
+++ RUN_MAIN.sh
@@ -1,3 +1,3 @@
 #bin/bash
 
-PYTHONPATH=. python src/main.py --config /home/cewinharhar/GITHUB/reincatalyze/src/CONFIG/config.yaml
+PYTHONPATH=. python src/main.py --config /home/cewinharhar/GITHUB/reincatalyze/src/CONFIG/config4.yaml
