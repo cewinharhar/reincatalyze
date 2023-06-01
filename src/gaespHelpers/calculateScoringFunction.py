9170d20da1ffd18ab61ed305567a084444ef1002
cewinharhar <kevin.yar@outlook.com>
2023-06-01 19:28:34 +0200

    still on mol2 in main gaesp

--- src/gaespHelpers/calculateScoringFunction.py
+++ src/gaespHelpers/calculateScoringFunction.py
@@ -2,6 +2,8 @@ from Bio.PDB import *
 from Bio.SVDSuperimposer import SVDSuperimposer
 import numpy as np
 
+import subprocess
+
 def align_proteins(reference_pdb, reference_ligand_pdbqt, reference_ligand_id,
                    target_protein_pdb, target_ligand_pdbqt, target_ligand_id, 
                    residues1, residues2, cofactor_ids, 
@@ -89,11 +91,11 @@ def align_proteins(reference_pdb, reference_ligand_pdbqt, reference_ligand_id,
 if __name__ == "__main__":
 
     dici = dict(
-        reference_pdb = 'data/raw/ortho12_FE_oxo.pdb', 
-        reference_ligand_pdbqt = "data/raw/ortho12_FE_oxo_sub9.pdbqt", 
+        reference_pdb = 'data/raw/test/ortho12_FE_oxo.pdb', 
+        reference_ligand_pdbqt = "/home/cewinharhar/GITHUB/reincatalyze/data/raw/test/akgd31_sub9_4_renamed.pdb", 
         reference_ligand_id = 3,
         target_protein_pdb = "data/raw/test/akgd31.pdb", 
-        target_ligand_pdbqt= "data/raw/test/akgd31_sub9.pdbqt", 
+        target_ligand_pdbqt= "data/raw/test/akgd31_sub94.pdb", 
         target_ligand_id = 3, 
         residues1 = [210, 268, 212], 
         residues2 = [167, 225, 196], 
@@ -125,3 +127,97 @@ if __name__ == "__main__":
                                       output_file=outputFile)
     print('Difference: ', difference)
     
+for i in reference_structure["A"].get_residues():
+    list(i.get_atoms())
+    break
+
+
+receptor = "/home/cewinharhar/GITHUB/reincatalyze/data/raw/test/akgd31.pdbqt"
+ligand4Cmd = "/home/cewinharhar/GITHUB/reincatalyze/data/raw/test/sub9.pdbqt"
+thread = 8000
+seed = 42
+cx = cy = cz = 0
+sx = sy = sz = 20
+ligandOutPath = "/home/cewinharhar/GITHUB/reincatalyze/data/raw/test/akgd31_sub9.pdbqt"
+num_modes = 5
+exhaustiveness = 16
+vina_gpu_cuda_path = "/home/cewinharhar/GITHUB/Vina-GPU-2.0/Vina-GPU+"
+
+vina_docking=f"./Vina-GPU --receptor {receptor} --ligand {ligand4Cmd} --thread {thread}\
+                --seed {seed} --center_x {cx} --center_y {cy} --center_z {cz}  \
+                --size_x {sx} --size_y {sy} --size_z {sz} \
+                --out {ligandOutPath} --num_modes {num_modes} --search_depth {exhaustiveness}"
+
+ps = subprocess.Popen([vina_docking],shell=True, cwd=vina_gpu_cuda_path, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+stdout, stderr = ps.communicate(timeout=100)
+
+pdbqt_to_pdb=f"./Vina-GPU --receptor {receptor} --ligand {ligand4Cmd} --thread {thread}\
+                --seed {seed} --center_x {cx} --center_y {cy} --center_z {cz}  \
+                --size_x {sx} --size_y {sy} --size_z {sz} \
+                --out {ligandOutPath} --num_modes {num_modes} --search_depth {exhaustiveness}"
+
+ps = subprocess.Popen([pdbqt_to_pdb],shell=True, stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+stdout, stderr = ps.communicate(timeout=100)
+
+
+def pdbqt_to_pdb(pdbqt_filename, pdb_filename):
+    with open(pdbqt_filename, 'r') as infile, open(pdb_filename, 'w') as outfile:
+        for line in infile:
+            if line.startswith(("ATOM", "HETATM")):
+                outfile.write(line[:66] + '\n')  # discard atom type and partial charge
+
+def rename_atoms(pdb_filename, pdb_output):
+    with open(pdb_filename, 'r') as infile, open(pdb_output, 'w') as outfile:
+        for line in infile:
+            if line.startswith(("ATOM", "HETATM")):
+                atom_name = line[12:16].strip()
+                atom_serial = int(line[6:11])
+                if atom_name == 'C':
+                    new_atom_name = 'C' + str(atom_serial)
+                    line = line[:12] + new_atom_name.ljust(4) + line[16:]
+                if atom_name == 'O':
+                    new_atom_name = 'O' + str(atom_serial)
+                    line = line[:12] + new_atom_name.ljust(4) + line[16:]
+                outfile.write(line)
+
+
+pdbqt_to_pdb(
+    "/home/cewinharhar/GITHUB/reincatalyze/data/raw/test/akgd31_sub9_4.pdbqt",
+    "/home/cewinharhar/GITHUB/reincatalyze/data/raw/test/akgd31_sub9_4.pdb"
+)
+
+rename_atoms(
+    "/home/cewinharhar/GITHUB/reincatalyze/data/raw/test/akgd31_sub9_4.pdb",
+    "/home/cewinharhar/GITHUB/reincatalyze/data/raw/test/akgd31_sub9_4_renamed.pdb"
+)
+    
+subSmiles = ['CC(=O)CCc1ccccc1', 'OC(=O)CCc1ccccc1', 'COc1cccc(CCC(O)=O)c1', 'COc1cc(CCCO)ccc1O', 'COc1cc(CCC(O)=O)ccc1O', 'OC(=O)CCc1ccc2OCOc2c1', 'COc1cc(CCCO)cc(OC)c1O', 'OC(=O)Cc1ccccc1', 'CC(=O)CCc1ccc2OCOc2c1']
+
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
+lig = Chem.MolFromSmiles("CC(=O)CCc1ccccc1")
+protonated_lig = Chem.AddHs(lig)
+Chem.AllChem.EmbedMolecule(protonated_lig)
+
+
+Chem.MolToPDBFile(protonated_lig, filename = f"/home/cewinharhar/GITHUB/reincatalyze/data/raw/test/dulc.pdb")
+
+Chem.MolToMolFile(protonated_lig, filename = f"{config.ligand_files}/ligand_{tmp}.mol")
+
+meeko_prep = MoleculePreparation()
+meeko_prep.prepare(protonated_lig)
+lig_pdbqt = meeko_prep.write_pdbqt_string()
+
+# Read the .mol file
+mol = Chem.MolFromMolFile("input.mol")
+
+# Add hydrogens
+mol = Chem.AddHs(mol)
+
+# Compute 3D coordinates§
+AllChem.EmbedMolecule(mol)
+
+# Write out a .pdb file
+AllChem.MolToPDBFile(mol, "output.pdb")
