28cd3e0fe708d3af95e3c5e66aeea8e21a7834c1
cewinharhar <kevin.yar@outlook.com>
2023-07-02 17:13:23 +0200

    ssh session

---
+++ run_deepMutSizeRep.sh
@@ -0,0 +1,1 @@
+PYTHONPATH=. python src/main.py --config /home/cewinharhar/GITHUB/reincatalyze/src/CONFIG/configX_deepMutSize_REP.yaml
---
+++ run_nrHiddenRep.sh
@@ -0,0 +1,1 @@
+PYTHONPATH=. python src/main.py --config /home/cewinharhar/GITHUB/reincatalyze/src/CONFIG/configX_nrHidden_REP.yaml
